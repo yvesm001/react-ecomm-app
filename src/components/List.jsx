219a53e175691bc78ab6eda2f4123e90fac775a8
Yves Martinez <yves.oliver.m@gmail.com>
2024-05-26 12:28:46 -0400

    Add item form successfully adds new item. Need to have form add item images and possibly generate unique item ids

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,3 +1,5 @@
+// THIS COMPONENT RENDERS THE LIST OF ITEMS AND THE FORM TO ADD AN ITEM
+
 import React, { useState } from "react";
 import itemsData from "../assets/data.json";
 import ProductCard from "./ProductCard";
@@ -5,10 +7,188 @@ import ProductCard from "./ProductCard";
 export default function List() {
   const [itemsList, setItemsList] = useState(itemsData);
 
-  
+  //USE STATES FOR ADD ITEM FORM
+  const [id, setId] = useState(0);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState(0);
+  const [discountPercentage, setDiscountPercentage] = useState(0);
+  const [rating, setRating] = useState(0);
+  const [stock, setStock] = useState(0);
+  const [brand, setbrand] = useState("");
+  const [category, setCategory] = useState("");
+  const [thumbnail, setThumbnail] = useState("");
+  //   const [images, setImages] = useState("");
+
+  //PREVENTING PAGE RELOAD WHEN CLICKING SUBMIT
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    //CREATING NEW ITEM
+    const newItem = {
+      id,
+      title,
+      description,
+      price,
+      discountPercentage,
+      rating,
+      stock,
+      brand,
+      category,
+      thumbnail,
+    };
+
+    //ADDING NEW ITEM TO ITEMLIST
+    setItemsList((prev) => [newItem, ...prev]);
+
+    //RESETTING VALUES
+    setId(0);
+    setTitle("");
+    setDescription("");
+    setPrice(0);
+    setDiscountPercentage(0);
+    setRating(0);
+    setStock(0);
+    setbrand("");
+    setCategory("");
+    setThumbnail("");
+  };
+
   return (
     <div>
-    
+      {/* FORM */}
+      <div className="form-div">
+        <form onSubmit={handleSubmit}>
+          <div>
+            <h1>Add an Item</h1>
+
+            {/* SET ITEM ID */}
+            <label>
+              ID
+              <input
+                name="id"
+                type="number"
+                placeholder="ID"
+                onChange={(event) => setId(event.target.value)}
+              />
+            </label>
+
+            {/* SET ITEM TITLE */}
+            <label>
+              Title
+              <input
+                name="title"
+                type="text"
+                placeholder="Title"
+                onChange={(event) => setTitle(event.target.value)}
+              />
+            </label>
+
+            {/* SET ITEM DESCRIPTION */}
+            <label>
+              Description
+              <input
+                name="description"
+                type="text"
+                placeholder="Description"
+                onChange={(event) => setDescription(event.target.value)}
+              />
+            </label>
+
+            {/* SET ITEM PRICE */}
+            <label>
+              Price
+              <input
+                name="price"
+                type="number"
+                placeholder="Price"
+                onChange={(event) => setPrice(event.target.value)}
+              />
+            </label>
+
+            {/* SET ITEM DISCOUNT PERCENTAGE */}
+            <label>
+              Discount Percentage
+              <input
+                name="discountPercentage"
+                type="number"
+                placeholder="Discount Percentage"
+                onChange={(event) => setDiscountPercentage(event.target.value)}
+              />
+            </label>
+
+            {/* SET ITEM RATING */}
+            <label>
+              Rating
+              <input
+                name="rating"
+                type="number"
+                placeholder="Rating"
+                onChange={(event) => setRating(event.target.value)}
+              />
+            </label>
+
+            {/* SET ITEM STOCK */}
+            <label>
+              Stock
+              <input
+                name="stock"
+                type="number"
+                placeholder="Stock"
+                onChange={(event) => setStock(event.target.value)}
+              />
+            </label>
+
+            {/* SET ITEM BRAND */}
+            <label>
+              Brand
+              <input
+                name="brand"
+                type="text"
+                placeholder="Brand"
+                onChange={(event) => setBrand(event.target.value)}
+              />
+            </label>
+
+            {/* SET ITEM CATEGORY */}
+            <label>
+              Category
+              <input
+                name="cateogry"
+                type="text"
+                placeholder="Category"
+                onChange={(event) => setCategory(event.target.value)}
+              />
+            </label>
+
+            {/* SET ITEM THUMBNAIL */}
+            <label>
+              Thumbnail
+              <input
+                name="thumbnail"
+                type="text"
+                placeholder="Thumbnail"
+                onChange={(event) => setThumbnail(event.target.value)}
+              />
+            </label>
+
+            {/* SET ITEM IMAGES */}
+            {/* <label>
+            Images
+            <input
+              name="images"
+              type="text"
+              placeholder="Images"
+              onChange={(event) => setImages(event.target.value)}
+            />
+          </label> */}
+
+            <button type="submit">Add Item</button>
+          </div>
+        </form>
+      </div>
+
+      {/* RENDER PRODUCTCARD FOR EACH ITEM TO DISPLAY LIST OF ITEMS */}
       <h2>Item List</h2>
       {itemsList.map((item) => {
         return <ProductCard key={item.id} item={item} />;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -4,17 +4,14 @@ import AddItem from "../components/AddItem";
 
 export default function Dashboard() {
   return (
-    <div className="add-item-div">
-      <AddItem />
       <div className="item-list">
         <List />
       </div>
-    </div>
   );
 }
 
-/* THIS PAGE RENDERS THE LIST COMPONENT. 
+/* THIS PAGE RENDERS THE LIST COMPONENT (WHICH ALSO CONTAINS ADD ITEM FORM)
    TO MAKE CHANGES TO THE WAY THE LIST IS DISPLAYED DO SO IN THE LIST COMPONENT FILE
    THE "ITEM-LIST" DIV CREATES A DIV IN THE CENTER OF THE PAGE THAT EXCLUDES THE DIMENSIONS OF NAVBAR/SIDEBAR/FOOTER  */
 
-//  NEEDS ADD NEW ITEM FORM COMPONENT
+
