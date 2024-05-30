f2fe75fcea65b66764d5f3a57b9f1d506af7c605
Yves Martinez <yves.oliver.m@gmail.com>
2024-05-30 16:18:33 -0400

    styled 404 page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,13 +1,15 @@
-const imgURL =
-  "https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/m3/react-routing/404.gif";
+import { Link } from "react-router-dom";
 
-function NotFound() {
+export default function NotFound() {
   return (
     <div className="not-found">
       <h1>404</h1>
-      <img src={imgURL} alt="404 error gif" className="page-img" />
+      <h2>PAGE NOT FOUND</h2>
+      <p>The page you're looking for might have been removed, <br>
+      </br>renamed or is temporarily unavailable.</p>
+        <Link to="/" className="button-link">
+        BACK TO HOME
+        </Link>
     </div>
   );
 }
-
-export default NotFound;
